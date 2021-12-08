72538035f3c44cfe09711fd294e1fc47165a84e8
Conot <ctuck002@gold.ac.uk>
2021-12-08 05:04:34 +0000

    Added API functionality

--- routes/main.js
+++ routes/main.js
@@ -1,12 +1,24 @@
-const { MongoClient } = require("mongodb");
+const MongoClient = require("mongodb").MongoClient;
+const uri = "mongodb+srv://sentiment:y4tKnECuRyhOI9Ml@cluster0.ogd7h.mongodb.net/test";
 
 module.exports = function(app)
-
 {
-
-//this renders nothing
-     app.get('/',function(req,res){
-        res.render('')
+     app.get('/getMinute',function(req,res){
+      MongoClient.connect(uri, async function(err, dbClient) {
+         if (err) throw err;
+         var db = dbClient.db('minuteSentiment');
+         try{
+            await db.collection('1min').find().toArray((findErr, results) =>{
+            if (findErr) throw findErr;
+            else
+            console.log("sent Minute collection");
+            res.json(results);
+            });
+         }
+         catch{
+           console.log("error collecting data!");
+         }
+         dbClient.close();                                                                                                                                                                                               
+       });
      });
-
-}
+   }
